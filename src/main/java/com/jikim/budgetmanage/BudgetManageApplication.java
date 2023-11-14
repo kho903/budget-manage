edb5a6ac2aa589509463f7be706bc45ec483e08f
kho903 <gmldnr2222@naver.com>
2023-11-14 14:29:15 +0900

    conf: start budget-manage project

---
+++ src/main/java/com/jikim/budgetmanage/BudgetManageApplication.java
@@ -0,0 +1,13 @@
+package com.jikim.budgetmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetManageApplication.class, args);
+	}
+
+}
